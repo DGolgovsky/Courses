7f4f4a88838a8e171a2691df468e6155eb90eac5
Dmitry Golgovsky <d.westcoast@aol.com>
2017-11-24 15:55:32 +0300

    Draft part 04 task

--- CPP.Part_2/week_5/04.MPL_Gen_Classes/get_size.cpp
+++ CPP.Part_2/week_5/04.MPL_Gen_Classes/get_size.cpp
@@ -1,22 +1,33 @@
-// Example program
+/* WARNING!
+ * Code doesn't work yet
+ */
+/*
+ * Напишите шаблонную функцию get_size, которая принимает
+ * значение некоторого типа, у которого есть
+ * либо константный метод size() возвращающий size_t,
+ * либо поле size типа size_t,
+ * и возвращает соответствующее значение.
+ */
+
 #include <iostream>
-#include <string>
 
 template <bool condition, typename Type>
 struct EnableIf;
- 
+
 template <typename Type>
 struct EnableIf<true, Type>
 {
     using type = Type;
 };
 
-template<typename T> struct has_size {
-private:  // Спрячем от пользователя детали реализации.
+template<typename T>
+struct has_size
+{
+private:
     static int detect(...);  // Статическую функцию и вызывать проще.
     template<typename U> static decltype(std::declval<U>().size(42)) detect(const U&);
 public:
-    static constexpr bool value = std::is_same<void, decltype(detect(std::declval<T>()))>::value;  // Вот видите, готово.
+    static constexpr bool value = std::is_same<void, decltype(detect(std::declval<T>()))>::value;
 };
 
 template <typename T>
@@ -25,7 +36,7 @@ get_size(T container)
 {
     return container.size();
 }
- 
+
 template <typename T>
 typename EnableIf<has_size<T>::value, size_t>::type
 get_size(T value)
@@ -38,15 +49,15 @@ int main()
     std::string s{"Hello"};
     size_t s_size = get_size(s);   // 5, вызывается метод size()
     std::cout << s_size << '\n';
-    
+
     struct Struct {
         size_t size = 0;
     };
-    
+
     Struct x{10};
     size_t x_size = get_size(x);  // 10, читается поле size
     std::cout << x_size << '\n';
-    
+
     return 0;
 }
 

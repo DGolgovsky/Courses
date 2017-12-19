66a32bb999691f8432c44b78126b5a0da7a0b688
Dmitry Golgovsky <d.westcoast@aol.com>
2017-12-19 16:54:26 +0300

    Sockets <incomplete>

---
+++ CPP_Multithread/3.1_Berkeley_Sockets/set_nonblock.h
@@ -0,0 +1,12 @@
+int set_nonblock(int fd)
+{
+    int flags;
+#if defined (O_NONBLOCK)
+    if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
+        flags = 0;
+    return fcntl(fd, F_SETFL, flags|O_NONBLOCK);
+#else
+    flags = 1;
+    return ioctl(fd, FIOBIO, &flags);
+#endif
+}
